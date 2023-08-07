d43cb53122b862b810ff582b55cdca6d2b0f327c
Raaj Kanchan <iamraajkanchan@gmail.com>
2023-08-07 23:53:57 +0530

    Minor changes in PaymentActivity.kt

--- app/src/main/java/com/razorpay/sampleapp/kotlin/PaymentActivity.kt
+++ app/src/main/java/com/razorpay/sampleapp/kotlin/PaymentActivity.kt
@@ -16,9 +16,10 @@ import com.razorpay.sampleapp.R
 import org.json.JSONObject
 import java.lang.Exception
 
-class PaymentActivity: Activity(), PaymentResultWithDataListener, ExternalWalletListener, DialogInterface.OnClickListener {
+class PaymentActivity : Activity(), PaymentResultWithDataListener, ExternalWalletListener,
+    DialogInterface.OnClickListener {
 
-    val TAG:String = PaymentActivity::class.toString()
+    val TAG: String = PaymentActivity::class.toString()
     private lateinit var alertDialogBuilder: AlertDialog.Builder
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +33,7 @@ class PaymentActivity: Activity(), PaymentResultWithDataListener, ExternalWallet
         alertDialogBuilder = AlertDialog.Builder(this@PaymentActivity)
         alertDialogBuilder.setTitle("Payment Result")
         alertDialogBuilder.setCancelable(true)
-        alertDialogBuilder.setPositiveButton("Ok",this)
+        alertDialogBuilder.setPositiveButton("Ok", this)
         val button: Button = findViewById(R.id.btn_pay)
         button.setOnClickListener {
             startPayment()
@@ -43,45 +44,45 @@ class PaymentActivity: Activity(), PaymentResultWithDataListener, ExternalWallet
         /*
         *  You need to pass current activity in order to let Razorpay create CheckoutActivity
         * */
-        val activity:Activity = this
+        val activity: Activity = this@PaymentActivity
         val co = Checkout()
         val etApiKey = findViewById<EditText>(R.id.et_api_key)
         val etCustomOptions = findViewById<EditText>(R.id.et_custom_options)
-        if (!TextUtils.isEmpty(etApiKey.text.toString())){
+        if (!TextUtils.isEmpty(etApiKey.text.toString())) {
             co.setKeyID(etApiKey.text.toString())
         }
         try {
             var options = JSONObject()
-            if (!TextUtils.isEmpty(etCustomOptions.text.toString())){
+            if (!TextUtils.isEmpty(etCustomOptions.text.toString())) {
                 options = JSONObject(etCustomOptions.text.toString())
-            }else{
-                options.put("name","Razorpay Corp")
-                options.put("description","Demoing Charges")
+            } else {
+                options.put("name", "Razorpay Corp")
+                options.put("description", "Demoing Charges")
                 //You can omit the image option to fetch the image from dashboard
-                options.put("image","https://s3.amazonaws.com/rzp-mobile/images/rzp.png")
-                options.put("currency","INR")
-                options.put("amount","100")
-                options.put("send_sms_hash",true);
+                options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png")
+                options.put("currency", "INR")
+                options.put("amount", "100")
+                options.put("send_sms_hash", true);
 
                 val prefill = JSONObject()
-                prefill.put("email","test@razorpay.com")
-                prefill.put("contact","9021066696")
+                prefill.put("email", "test@razorpay.com")
+                prefill.put("contact", "7718881920")
 
-                options.put("prefill",prefill)
+                options.put("prefill", prefill)
             }
 
-            co.open(activity,options)
-        }catch (e: Exception){
-            Toast.makeText(activity,"Error in payment: "+ e.message,Toast.LENGTH_LONG).show()
+            co.open(activity, options)
+        } catch (e: Exception) {
+            Toast.makeText(activity, "Error in payment: " + e.message, Toast.LENGTH_LONG).show()
             e.printStackTrace()
         }
     }
 
     override fun onPaymentSuccess(p0: String?, p1: PaymentData?) {
-        try{
+        try {
             alertDialogBuilder.setMessage("Payment Successful : Payment ID: $p0\nPayment Data: ${p1?.data}")
             alertDialogBuilder.show()
-        }catch (e: Exception){
+        } catch (e: Exception) {
             e.printStackTrace()
         }
     }
@@ -90,16 +91,16 @@ class PaymentActivity: Activity(), PaymentResultWithDataListener, ExternalWallet
         try {
             alertDialogBuilder.setMessage("Payment Failed : Payment Data: ${p2?.data}")
             alertDialogBuilder.show()
-        }catch (e: Exception){
+        } catch (e: Exception) {
             e.printStackTrace()
         }
     }
 
     override fun onExternalWalletSelected(p0: String?, p1: PaymentData?) {
-        try{
+        try {
             alertDialogBuilder.setMessage("External wallet was selected : Payment Data: ${p1?.data}")
             alertDialogBuilder.show()
-        }catch (e: Exception){
+        } catch (e: Exception) {
             e.printStackTrace()
         }
     }
